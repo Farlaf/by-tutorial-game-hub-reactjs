72583d3ffe77e68e93973c09ee97e51b48630b0f
Farlaf <themehills@gmail.com>
2023-03-25 12:35:13 +0400

    Adding a Dynamic Heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
@@ -49,22 +50,25 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area="main">
-                <Flex paddingLeft={2} paddingBottom={5}>
-                    <Box marginRight={5}>
-                        <PlatformSelector
-                            selectedPlatform={gameQuery.platform}
-                            onSelectPlatform={(platform) =>
-                                setGameQuery({ ...gameQuery, platform })
+                <Box paddingLeft={2}>
+                    <GameHeading gameQuery={gameQuery} />
+                    <Flex paddingBottom={5}>
+                        <Box marginRight={5}>
+                            <PlatformSelector
+                                selectedPlatform={gameQuery.platform}
+                                onSelectPlatform={(platform) =>
+                                    setGameQuery({ ...gameQuery, platform })
+                                }
+                            />
+                        </Box>
+                        <SortSelector
+                            sortOrder={gameQuery.sortOrder}
+                            onSelectSortOrder={(order) =>
+                                setGameQuery({ ...gameQuery, sortOrder: order })
                             }
                         />
-                    </Box>
-                    <SortSelector
-                        sortOrder={gameQuery.sortOrder}
-                        onSelectSortOrder={(order) =>
-                            setGameQuery({ ...gameQuery, sortOrder: order })
-                        }
-                    />
-                </Flex>
+                    </Flex>
+                </Box>
                 <GameGrid gameQuery={gameQuery} />
             </GridItem>
         </Grid>
