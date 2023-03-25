13fa28a4e83bc0fd5bc76963d4e7706de8b2178d
Farlaf <themehills@gmail.com>
2023-03-25 10:00:10 +0400

    Filtering Games by Platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,20 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
+import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
 
 interface Props {
+    selectedPlatform: Platform | null;
     selectedGenre: Genre | null;
 }
 
-const GameGrid = ({ selectedGenre }: Props) => {
-    const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
+    const { data, error, isLoading } = useGames(
+        selectedGenre,
+        selectedPlatform
+    );
     const skeletons = [1, 2, 3, 4, 5, 6];
 
     return (
@@ -32,11 +36,12 @@ const GameGrid = ({ selectedGenre }: Props) => {
                             <GameCardSkeleton />
                         </GameCardContainer>
                     ))}
-                {data.map((game) => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
-                    </GameCardContainer>
-                ))}
+                {!isLoading &&
+                    data.map((game) => (
+                        <GameCardContainer key={game.id}>
+                            <GameCard game={game} />
+                        </GameCardContainer>
+                    ))}
             </SimpleGrid>
         </>
     );
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,20 +1,35 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-const PlatformSelector = () => {
+interface Props {
+    selectedPlatform: Platform | null;
+    onSelectPlatform: (platform: Platform) => void;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
     const { data, error } = usePlatforms();
 
     if (error) return null;
 
     return (
         <Menu>
-            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-                Platform
+            <MenuButton
+                marginX="10px"
+                as={Button}
+                rightIcon={<BsChevronDown />}
+            >
+                {selectedPlatform?.name || "Platform"}
             </MenuButton>
             <MenuList>
                 {data.map((platform) => (
-                    <MenuItem key={platform.id}>{platform.name}</MenuItem>
+                    <MenuItem
+                        onClick={() => onSelectPlatform(platform)}
+                        key={platform.id}
+                    >
+                        {platform.name}
+                    </MenuItem>
                 ))}
             </MenuList>
         </Menu>
