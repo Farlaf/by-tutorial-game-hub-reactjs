0e903ab81ea2290090fb23ce7876a9e73d4d17d1
Farlaf <themehills@gmail.com>
2023-05-05 18:42:31 +0400

    infinite scroll

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -15,7 +15,7 @@ export interface Platform {
 const usePlatforms = () =>
     useQuery({
         queryKey: ["platforms"],
-        queryFn: apiClient.getAll,
+        queryFn: () => apiClient.getAll,
         staleTime: 24 * 60 * 60 * 1000, // 24h
         initialData: { count: platforms.length, results: platforms },
     });
