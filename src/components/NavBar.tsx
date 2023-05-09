23d3d8d9bf709c2296ced5d00044929c0ff3bf19
Farlaf <themehills@gmail.com>
2023-05-09 17:44:01 +0400

    Fix nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,11 +7,10 @@ import SearchInput from "./SearchInput";
 const NavBar = () => {
     return (
         <HStack padding="10px">
-            <Box boxSize="60px">
-                <Link to="/">
-                    <Image src={logo} />
-                </Link>
-            </Box>
+            <Link to="/">
+                <Image src={logo} boxSize="60px" objectFit="cover" />
+            </Link>
+
             <SearchInput />
             <ColorModeSwitch />
         </HStack>
