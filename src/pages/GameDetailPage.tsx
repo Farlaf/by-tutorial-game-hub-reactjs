20c237e27ee0df995315f64bd48edf7dd72cd643
Farlaf <themehills@gmail.com>
2023-05-09 16:28:45 +0400

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,6 +2,7 @@ import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameScreenshots from "../components/GameScreenshots";
 import GameTrailers from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
@@ -15,6 +16,7 @@ const GameDetailPage = () => {
 
     return (
         <>
+            <GameScreenshots gameId={game.id} />
             <Heading>{game.name}</Heading>
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game} />
